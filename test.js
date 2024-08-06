5e586ef653093b7072c9ca157663d35cc26d34e6
Olusegun Francis Akindipe <akinfergie@gmail.com>
2024-08-07 00:52:32 +0100

    latest change

--- test.js
+++ test.js
@@ -1,11 +1,8 @@
 const test = 2 + 2;
 return test;
-<<<<<<< HEAD
-=======
 
 const secondTest = 4 + 4;
 return secondTest;
 
 const anotherTest = 4 + 4;
 return anotherTest;
->>>>>>> parent of 4720368 (third test)
