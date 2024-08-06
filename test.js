1a84f96876edd6329430f823db346aab19805d6b
Olusegun Francis Akindipe <akinfergie@gmail.com>
2024-08-07 00:43:48 +0100

    Revert "third test"
    This reverts commit 4720368d0915155a5a8279036199e855a2f5cfb5.

--- test.js
+++ test.js
@@ -1,2 +1,11 @@
 const test = 2 + 2;
 return test;
+<<<<<<< HEAD
+=======
+
+const secondTest = 4 + 4;
+return secondTest;
+
+const anotherTest = 4 + 4;
+return anotherTest;
+>>>>>>> parent of 4720368 (third test)
